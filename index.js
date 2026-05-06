e7e0c78e1a806e34d3cbbeeee1dd1ba8473c33be
abbaxi786 <141753795+abbaxi786@users.noreply.github.com>
2026-05-06 21:00:34 +0500

    The cors setting to normal

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://bullish-ecommerce-frontend.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://bullish-ecommerce-frontend.vercel.app"
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
+app.use(cors());
 
 app.use(express.json());
 
